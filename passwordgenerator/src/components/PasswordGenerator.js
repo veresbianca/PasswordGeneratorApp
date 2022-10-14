84a53b992bd28e4c05a3d702f18f1a1dd009ace8
Răzvan Sbîngu <razvan.sbingu@thinslices.com>
2022-10-14 15:08:49 +0300

    code review, ep02

--- passwordgenerator/src/components/PasswordGenerator.js
+++ passwordgenerator/src/components/PasswordGenerator.js
@@ -26,24 +26,51 @@ const passwordPlaceholder = "P4$5W0rD!";
 
 function PasswordGenerator() {
   const [formInputData, setFormInputData] = useState(initialValues);
-  const [currentPassword, setPassword] = useState(passwordPlaceholder);
+
+  /*
+   * Givent that this piece of state is used only to be passed
+   * as a `prop`, it can safely be moved inside that respective component
+   *
+   * What is needed for the computation of the password strenght is
+   * ONLY the `currentPassword`
+   */
   const [strengthStatus, setStrengthStatus] = useState("default");
+
+  /*
+   * It's a best practice to make the single difference
+   * between a state variable and its setter only the `set` prefix.
+   *
+   * e.g. counter, setCounter
+   */
+  const [currentPassword, setPassword] = useState(passwordPlaceholder);
   const [isPassCopied, setCopiedPassStatus] = useState(false);
+
+  /*
+   * This can be declared as a `const
+   * because it's derived state. When the state changes,
+   * so does this variable.
+   */
   let isEmptyPass = currentPassword === "P4$5W0rD!";
 
-  const handleFormSubmit = (event) => {
+  const handleFormSubmit = event => {
     event.preventDefault();
 
     setPassword(generatePassword());
     setCopiedPassStatus(false);
+
+    /*
+     * Setting the value for this variable is not necessary
+     * because it's derived state
+     */
     isEmptyPass = false;
     setStrengthStatus(checkPasswordStrength(generatePassword()));
   };
 
-  const checkPasswordStrength = (password) => {
-    const strong = new RegExp(
-      "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,})"
-    );
+  /*
+   * This function has no value in being declared in this component
+   */
+  const checkPasswordStrength = password => {
+    const strong = new RegExp("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,})");
 
     const medium = new RegExp(
       "^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})"
@@ -53,6 +80,11 @@ function PasswordGenerator() {
       "(?=.*[a-z])(?=.{6,})|(?=.*[A-Z])(?=.{6,})|(?=.*[0-9])(?=.{6,})|(?=.*[^A-Za-z0-9])(?=.{6,})"
     );
 
+    /*
+     * Given that you're using the early return pattern,
+     * you can safely remove the `else` keywords in order
+     * to improve readability
+     */
     if (strong.test(password)) {
       return "strong";
     } else if (medium.test(password)) {
@@ -67,14 +99,24 @@ function PasswordGenerator() {
   const generatePassword = () => {
     let password = "";
 
+    /*
+     * Pentru fiecare index din lungimea parolei,
+     * Vreau sa generez un caracter luand in considerare
+     * valorile criteriilor selectate (lowercase, UPPERCASE, numar, simbol)
+     *
+     * Aleg aleatoriu criteriu pentru care generez caracterul,
+     * dar e necesar sa existe minim cate un caracter pentru
+     * fiecare criteriu selectat
+     */
+
     for (let i = 0; i < parseInt(formInputData.characterLength); ) {
       const criteria = Object.keys(formInputData).filter(
-        (key) => typeof formInputData[key] === "boolean" && formInputData[key]
+        key => typeof formInputData[key] === "boolean" && formInputData[key]
       );
 
       if (criteria.length) {
         // eslint-disable-next-line no-loop-func
-        criteria.forEach((criterion) => {
+        criteria.forEach(criterion => {
           if (i < parseInt(formInputData.characterLength)) {
             password += getRandomCharacter(passwordConfiguration[criterion]);
             i++;
